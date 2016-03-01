c440407c63b4751f7d3f948ca8ad814b3852cb6e
Vanderson Maroni <vandersongmaroni@gmail.com>
2016-03-01 18:57:11 -0300

    Terminando página de destaque, mas sem lógica

--- WebContent/console.js
+++ WebContent/console.js
@@ -13,6 +13,31 @@ function statusDisplay(data) {
 	}
 }
 
+function novo() {
+	var i = 0;
+	var form = document.getElementsByClassName("formulario-incluir");
+	for (i = 0; i < form.length; i++) {
+		form[i].style.display = "block";
+	}
+	document.getElementById("novo").style.display = "none";
+}
+
+function cancelar() {
+	var i = 0;
+	var form = document.getElementsByClassName("formulario-incluir");
+	var formAlterar = document.getElementsByClassName("formulario-alterar");
+	for (i = 0; i < form.length; i++) {
+		form[i].style.display = "none";
+		form[i].style.value = "";
+	}
+	for (i = 0; i < formAlterar.length; i++) {
+		formAlterar[i].style.display = "none";
+		formAlterar[i].style.value = "";
+	}
+	
+	document.getElementById("novo").style.display = "block";
+}
+
 var xmlhttp = new XMLHttpRequest();
 var url = base_url + "/destaques";
 var token = pegar_token();
@@ -26,16 +51,15 @@ html += "<tr><th>ID</th><th>Titulo</th><th>Descricao</th><th>Status</th><th>Data
 xmlhttp.onload = function(e) {
 	var obj = JSON.parse(xmlhttp.responseText);
 	for (key in obj) {
-		console.log(obj[key]);
-		html += "<tr>" + "<td>" + obj[key].id + "</td>" +
-		"<td>" + obj[key].titulo + "</td>" +
-		"<td>" + obj[key].descricao + "</td>" +
-		"<td>" + statusDisplay(obj[key].status) + "</td>" +
-		"<td>" + obj[key].dataCadastro + "</td>" +
-		"</tr>";
+		html += "<tr>" + "<td>" + obj[key].id + "</td>" + "<td>"
+				+ obj[key].titulo + "</td>" + "<td>" + obj[key].descricao
+				+ "</td>" + "<td>" + statusDisplay(obj[key].status) + "</td>"
+				+ "<td>" + obj[key].dataCadastro + "</td>" + "</tr>";
 	}
 
 	document.getElementById("tabela-destaque").innerHTML = html;
 }
 
 xmlhttp.send();
+
+
